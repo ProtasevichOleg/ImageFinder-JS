a692c4fe40c397dc668ec7de22099b5c5526bfc2
Oleh Protasevych <protasevicholeg@gmail.com>
2023-10-23 10:43:44 +0300

    regenerate gh-pages

--- src/jsModules/components/loadMoreButton.js
+++
@@ -1,9 +0,0 @@
-import { refs } from '../..';
-
-export function showLoadMoreButton() {
-  refs.loadMoreBtnEl.classList.remove('hidden');
-}
-
-export function hideLoadMoreButton() {
-  refs.loadMoreBtnEl.classList.add('hidden');
-}
